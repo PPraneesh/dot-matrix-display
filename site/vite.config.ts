44b7cad50e79519b349d702558eebb054df4ec48
Praneesh <parshipraneesh8@gmail.com>
2025-02-14 19:50:24 +0530

    fix

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/docs/',
+  base: '/dot-matrix-display/',
   plugins: [react()],
   build: {
-    outDir: '../docs',
+    outDir: 'docs', // Ensure this is correctly set to 'docs' for GitHub Pages
   },
 });
